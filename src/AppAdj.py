60f4e843f0fece1b4d6c2a65a6b971f05ebe9d9b
Julie Weeds <julie.weeds@gmail.com>
2013-06-06 08:53:35 +0100

    code to correlate sim scores

--- src/AppAdj.py
+++ src/AppAdj.py
@@ -3,7 +3,9 @@ __author__ = 'juliewe'
 import conf
 import sys
 import math
-
+import numpy
+import scipy.stats as stats
+import matplotlib.pyplot as plt
 
 def makesimlist(list):
     simlist = []
@@ -13,9 +15,17 @@ def makesimlist(list):
         simlist.append((score,id))
     return simlist
 
+def mymean(total,count):
+    return float(total)/float(count)
+
+def mysd(squaretotal,total,count):
+    mean = mymean(total,count)
+    var=float(squaretotal)/float(count)-mean*mean
+    return (mean,math.pow(var,0.5))
+
 class Entry:
 
-    def __init__(self,word,freq,width):
+    def __init__(self,word,freq,width,k):
         self.word=word
         self.freq=freq
         self.width=width
@@ -28,6 +38,8 @@ class Entry:
         self.nosims=0
         self.totalsims=0
         self.totalsquares=0
+        self.analysed=False
+        self.k=k
 
 
     def updatesims(self,list):
@@ -56,10 +68,11 @@ class Entry:
         while todo>0:
             self.sims.pop()
             todo-=1
+        self.k=k
 
     def display(self):
         print self.word+"\t"+"\t"+str(self.freq)+"\t"+str(self.width)
-        print self.sims
+        #print self.sims
         if self.nosims>0:
             print self.nosims, self.maxsim, self.avsim, self.sdsim
         print ""
@@ -71,14 +84,18 @@ class Entry:
         outstream.write("\n")
 
     def analyse(self):
-        for (score,neigh) in self.sims:
-            if score > self.maxsim:
-                self.maxsim=score
-            self.totalsims+=score
-            self.totalsquares+=score*score
-            self.nosims+=1
-        self.avsim=float(self.totalsims)/float(self.nosims)
-        self.sdsim=math.pow((float(self.totalsquares)/float(self.nosims)) - (self.avsim * self.avsim),0.5)
+        if self.analysed==False:
+            for (score,neigh) in self.sims:
+                if score > self.maxsim:
+                    self.maxsim=score
+                self.totalsims+=score
+                self.totalsquares+=score*score
+                self.nosims+=1
+            (self.avsim,self.sdsim)=mysd(self.totalsquares,self.totalsims,self.k)
+        self.analysed=True
+
+    def garbage(self):
+        self.sims=[] #remove list of tuples once processed to free memory
 
 class SimMatrix:
 
@@ -91,13 +108,18 @@ class SimMatrix:
         self.entrydict = {} # word --> entry
         self.k=k
         self.maxwidth=0
+        self.maxfreq=0
         self.adj_constant=0
         self.adjust_flag=flag #whether to adjust similarities
         self.testing=testing
+        self.filteredS=["___FILTERED___"]
+
         if self.testing:
-            self.batchcount=10
+            self.batchcount=1000
         else:
             self.batchcount=1000
+
+        self.analysed=False #have the entries been analysed statistically
 
         #read files
         self.readtotals()
@@ -115,13 +137,19 @@ class SimMatrix:
             l=line.rstrip()
             fields=l.split("\t")
             if len(fields)==3: #valid data line
+
                 word=fields[0]
-                freq=int(fields[1])
-                width=int(fields[2])
-                self.entrydict[word]=Entry(word,freq,width)
-                if width > self.maxwidth:
-                    self.maxwidth=width
-                linesread+=1
+                if word in self.filteredS:
+                    print "Excluding entry for "+word
+                else:
+                    freq=int(fields[1])
+                    width=int(fields[2])
+                    self.entrydict[word]=Entry(word,freq,width,self.k)
+                    if width > self.maxwidth:
+                        self.maxwidth=width
+                    if freq > self.maxfreq:
+                        self.maxfreq=freq
+                    linesread+=1
 
         instream.close()
         print "Read "+str(linesread)+" lines"
@@ -137,7 +165,9 @@ class SimMatrix:
         outfile=self.dir+self.outfile
         neighfile=self.dir+self.neighfile
         instream=open(filename,'r')
-        outstream=open(outfile,'w')
+        if self.adjust_flag:
+            outstream=open(outfile,'w')
+            neighstream=open(neighfile,'w')
 
         print "Reading "+filename
         linesread=0
@@ -147,19 +177,30 @@ class SimMatrix:
             fields=l.split("\t")
             fields.reverse()
             word=str(fields.pop())
-            if word in self.entrydict.keys():
-                self.adjust(word,fields)  #adjust and add similarities
-                self.entrydict[word].output(outstream)          #write sims output file
+            if word in self.filteredS:
+                print "Excluding entry for "+word
             else:
-                print "Error - no entry for word "+word
-                exit(1)
+                if word in self.entrydict.keys():
+                    self.adjust(word,fields)  #adjust and add similarities
+                    if self.adjust_flag:
+                        self.entrydict[word].output(outstream)          #write sims output file if sims are adjusted
+                    if self.entrydict[word].nosims>self.k:
+                        self.entrydict[word].topk(self.k)   #keep only top k neighbours
+                    if self.adjust_flag:
+                        self.entrydict[word].display()  #display
+                        self.entrydict[word].output(neighstream) #write neigh outputfile if sims are adjusted
+                    self.entrydict[word].analyse() #compute stats for entry
+                    self.entrydict[word].garbage()  #free memory
+                else:
+                    print "Error - no entry for word "+word
+                    exit(1)
             if linesread%self.batchcount==0:
                 print "Processed "+str(linesread)+" lines"
                 if self.testing:
                     break
-
-
-        outstream.close()
+        if self.adjust_flag:
+            neighstream.close()
+            outstream.close()
         instream.close()
         print "Finished reading input."
 
@@ -167,7 +208,7 @@ class SimMatrix:
 
         print "Generating neighbour file...."
 
-        neighstream=open(neighfile,'w')
+        neighstream=open(self.dir+self.neighfile,'w')
         done=0
         for word in self.entrydict.keys():
             if len(self.entrydict[word].sims)>0:
@@ -199,6 +240,80 @@ class SimMatrix:
             else:
                 entry.sims.append((score,neigh))
 
+    def analyse(self):
+        count=0
+        maxtotal=0
+        maxsquaretotal=0
+        avtotal=0
+        avsquaretotal=0
+        grandtotal=0
+        grandtotalcount=0
+        grandsquaretotal=0
+
+        for entry in self.entrydict.values():
+            if entry.nosims>0:
+                entry.analyse()
+                #entry.display()
+                count+=1
+                maxtotal+=entry.maxsim
+                maxsquaretotal+=entry.maxsim*entry.maxsim
+                avtotal+=entry.avsim
+                avsquaretotal+=entry.avsim*entry.avsim
+                grandtotal+=entry.totalsims
+                grandsquaretotal+=entry.totalsquares
+                grandtotalcount+=entry.k
+
+        (maxmean,maxsd)=mysd(maxsquaretotal,maxtotal,count)
+        (avmean,avsd)=mysd(avsquaretotal,avtotal,count)
+        (grandmean,grandsd)=mysd(grandsquaretotal,grandtotal,grandtotalcount)
+        print "For nearest neighbour, mean similarity = "+str(maxmean)+", sd ="+str(maxsd)
+        print "For all neighbours (k="+str(self.k)+"), mean mean similarity = "+str(avmean)+", sd = "+str(avsd)
+        print "For all entries (n="+str(count)+"), mean similarity = "+str(grandmean)+", sd = "+str(grandsd)
+        self.analysed=True
+
+    def correlate(self):
+        if self.analysed==False:
+            self.analyse()
+        x1s=[]
+        x2s=[]
+        y1s=[]
+        y2s=[]
+        name=[] #array of names associated with numpy arrays
+        array=[]#array of numpy arrays
+        for entry in self.entrydict.values():
+            if entry.nosims>0:
+                x1s.append(entry.width)
+                x2s.append(entry.freq)
+                y1s.append(entry.maxsim)
+                y2s.append(entry.avsim)
+        array=[numpy.array(x1s),numpy.array(x2s),numpy.array(y1s),numpy.array(y2s)]
+        name=["width","freq","maxsim","avsim"]
+        limit=[self.maxwidth,self.maxfreq,1,1]
+
+
+        for i in range(len(name)):
+            for j in range(len(name)):
+                if j>i:
+                    thispoly= numpy.poly1d(numpy.polyfit(array[i],array[j],1))
+
+                    pr=stats.spearmanr(array[i],array[j])
+                    mytitle = name[j]+" against "+name[i]
+                    print mytitle
+                    print "Spearman's r = "+str(pr[0])+" with p value = "+str(pr[1])
+                    print thispoly
+                    self.showpoly(array[i],array[j],thispoly,mytitle,pr,limit[i],limit[j])
+
+    def showpoly(self,x,y,poly,title,pr,xl,yl):
+        xp=numpy.linspace(0,xl,100)
+        plt.plot(x,y,'.',xp,poly(xp),'-')
+        plt.ylim(0,yl)
+        plt.title(title)
+        mytext1="srcc = "+str(pr[0])
+        mytext2="p = "+str(pr[1])
+        plt.text(0.05,yl*0.9,mytext1)
+        plt.text(0.05,yl*0.8,mytext2)
+        plt.show()
+
 
 if __name__ =="__main__":
     parameters = conf.configure(sys.argv)
@@ -208,5 +323,5 @@ if __name__ =="__main__":
     else:
         simsfile="sims"
     mymatrix= SimMatrix(parameters["directory"],parameters["k"],parameters["adjust_flag"],simsfile,parameters["testing"])
-    mymatrix.output_neighs()
+   # mymatrix.output_neighs()
 
--- src/Correlate.py
+++ src/Correlate.py
@@ -4,11 +4,8 @@ import sys, conf, AppAdj
 if __name__=="__main__":
     parameters = conf.configure(sys.argv)
     parameters["adjust_flag"]=False
-    simsfile="neighbours.strings"
+    simsfile="sims.adj.neighbours"
     mymatrix= AppAdj.SimMatrix(parameters["directory"],parameters["k"],parameters["adjust_flag"],simsfile,parameters["testing"])
 
-    for entry in mymatrix.entrydict.values():
-        if len(entry.sims)>0:
-
-            entry.analyse()
-            entry.display()
+    mymatrix.analyse()
+    mymatrix.correlate()
